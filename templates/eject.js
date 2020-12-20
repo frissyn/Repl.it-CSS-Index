d238247168982731ce2d3596c6f03f13d96c26e5
IreTheKID <62220201+IreTheKID@users.noreply.github.com>
2020-12-19 23:59:44 -0500

    fix semicolon

--- templates/eject.js
+++ templates/eject.js
@@ -1,5 +1,5 @@
 javascript:(function() {
-    var go = confirm(`Eject CSS from ${window.location.href}?`)
+    var go = confirm(`Eject CSS from ${window.location.href}?`);
     if (go) {
         var style = document.getElementById("injected-css");
         style.remove();
